93ef2b338440560b5f590647223e92829d379ab8
blocktrest <blocktrest@gmail.com>
2023-01-05 04:01:16 +0100

    updated metadata

--- resources/views/layouts/partials/guest/footer.blade.php
+++ resources/views/layouts/partials/guest/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-xl-2 col-lg-2 col-md-3 col-sm-3 col-12">
                 <div class="footer-logo">
-                    <a href="{{ url('/') }}">ITARE</a>
+                    <a href="{{ url('/') }}">ILERA</a>
                 </div>
             </div>
             <div class="col-xl-10 col-lg-10 col-md-9 col-sm-9 col-12">
@@ -17,7 +17,7 @@
             <div class="col-xl-6 col-sm-6">
                 <div class="copyright-text">
                     <p> <i class="fa fa-location-arrow"></i> Lagos, Nigeria</p>
-                    <p> Ilare MetaHackathon &copy; 2022. All Rights Reserved.</p>
+                    <p> Ilera MetaHackathon &copy; 2022. All Rights Reserved.</p>
                 </div>
             </div>
             <div class="col-xl-6 col-sm-6">
--- resources/views/layouts/partials/guest/navbar.blade.php
+++ resources/views/layouts/partials/guest/navbar.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
 
             <a class="navbar-brand" href="{{ url('/') }}" style="font-weight: 900">
-                ITARE
+                ILERA
             </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
